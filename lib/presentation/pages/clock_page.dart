0909e4a812fade445c97920f50679e17cbb44fba
Damodar Lohani <dlohani48@gmail.com>
2020-08-29 15:50:28 +0545

    step 1

--- lib/presentation/pages/clock_page.dart
+++ lib/presentation/pages/clock_page.dart
@@ -3,19 +3,11 @@ import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutter_worldtime/data/model/time_info.dart';
-import 'package:flutter_worldtime/data/service/worldtime_api.dart';
-import 'package:flutter_worldtime/presentation/notifiers/clocks_notifier.dart';
 import 'package:flutter_worldtime/presentation/widgets/clock_container.dart';
 import 'package:flutter_worldtime/presentation/widgets/clock_hands.dart';
-import 'package:flutter_worldtime/res/routes.dart';
 import 'package:intl/intl.dart';
 
 final timeProvider = StateProvider<DateTime>((ref) => DateTime.now());
-
-final timezone = FutureProvider<TimeInfo>((ref) async {
-  return WorldTimeApi.getCurrentTime();
-});
 
 class ClockPage extends StatefulWidget {
   @override
@@ -47,20 +39,6 @@ class _ClockPageState extends State<ClockPage> {
         child: ListView(
           padding: const EdgeInsets.all(16.0),
           children: <Widget>[
-            Row(
-              children: [
-                Spacer(),
-                MaterialButton(
-                  padding: const EdgeInsets.all(8.0),
-                  color: Colors.pink,
-                  shape: CircleBorder(),
-                  child: Icon(Icons.add),
-                  onPressed: () {
-                    Navigator.pushNamed(context, AppRoutes.timezonesPage);
-                  },
-                ),
-              ],
-            ),
             Container(
               child: Consumer((context, watch) {
                 final time = watch(timeProvider).state;
@@ -72,20 +50,6 @@ class _ClockPageState extends State<ClockPage> {
                       ),
                     ),
                     const SizedBox(height: 20.0),
-                    Consumer((context, watch) {
-                      final timeinfo = watch(timezone);
-                      return timeinfo.when(
-                          data: (timeinfo) => Text(
-                                timeinfo?.timezone ?? '',
-                                style: TextStyle(
-                                  color: Colors.pink,
-                                  fontWeight: FontWeight.w500,
-                                  fontSize: 24.0,
-                                ),
-                              ),
-                          loading: () => const CircularProgressIndicator(),
-                          error: (err, stack) => Container());
-                    }),
                     Text(
                       DateFormat.jm().format(time),
                       style: Theme.of(context).textTheme.headline3,
@@ -94,88 +58,6 @@ class _ClockPageState extends State<ClockPage> {
                   ],
                 );
               }),
-            ),
-            SizedBox(
-              height: 160,
-              child: Consumer(
-                (context, watch) {
-                  List<TimeInfo> clocks = watch(clocksProvider.state);
-                  var time2 = watch(timeProvider).state;
-                  if (clocks != null && clocks.isNotEmpty)
-                    return ListView.builder(
-                      itemCount: clocks.length,
-                      scrollDirection: Axis.horizontal,
-                      itemBuilder: (context, index) {
-                        TimeInfo clock = clocks[index];
-                        var time = time2.toUtc();
-                        int offset = clock.rawOffset + clock.dstOffset;
-                        if (clock.rawOffset > 0) {
-                          time = time.add(Duration(seconds: offset));
-                        } else if (clock.rawOffset < 0) {
-                          time =
-                              time.subtract(Duration(seconds: (offset * -1)));
-                        }
-                        return Card(
-                          child: Stack(
-                            children: [
-                              Container(
-                                width: 300,
-                                padding: const EdgeInsets.all(32.0),
-                                child: Column(
-                                  crossAxisAlignment:
-                                      CrossAxisAlignment.stretch,
-                                  children: [
-                                    Text(
-                                      clock.timezone,
-                                      style: TextStyle(
-                                        color: Colors.white,
-                                        fontWeight: FontWeight.w500,
-                                        fontSize: 20.0,
-                                      ),
-                                    ),
-                                    const SizedBox(height: 5.0),
-                                    Text(
-                                      "${clock.utcOffset}",
-                                      style: TextStyle(
-                                        color: Colors.grey,
-                                      ),
-                                    ),
-                                    const SizedBox(height: 15.0),
-                                    Row(
-                                      children: [
-                                        Expanded(
-                                          child: Text(
-                                              DateFormat.yMMMd().format(time)),
-                                        ),
-                                        Text(
-                                          DateFormat.jm().format(time),
-                                          textAlign: TextAlign.right,
-                                          style: TextStyle(
-                                            fontSize: 24.0,
-                                          ),
-                                        ),
-                                      ],
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              Align(
-                                alignment: Alignment.topLeft,
-                                child: IconButton(
-                                  icon: Icon(Icons.clear),
-                                  onPressed: () {
-                                    context.read(clocksProvider).remove(clock);
-                                  },
-                                ),
-                              )
-                            ],
-                          ),
-                        );
-                      },
-                    );
-                  return Container();
-                },
-              ),
             ),
           ],
         ),
